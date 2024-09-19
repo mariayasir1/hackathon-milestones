93213028993d7d55b2b5c882c080c1f24b4b4916
Maria Yasir <160783150+mariayasir1@users.noreply.github.com>
2024-09-19 11:25:30 -0700

    Add files via upload

---
+++ milestone1/script.js
@@ -0,0 +1,10 @@
+var Button = document.getElementById('button');
+var skills = document.getElementById('skills');
+Button.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
